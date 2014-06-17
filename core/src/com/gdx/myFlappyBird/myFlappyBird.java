2a1ada16828dce4e017c8b01178b26954ffd8de6
Vineeta Khatuja <vineetak@static-119.220.98.14-tataidc.co.in>
2014-06-17 15:35:27 +0530

    Modularized the code added a new class for bird

--- core/src/com/gdx/myFlappyBird/myFlappyBird.java
+++ core/src/com/gdx/myFlappyBird/myFlappyBird.java
@@ -12,9 +12,9 @@ import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 
 public class myFlappyBird extends ApplicationAdapter {
+	
 	SpriteBatch batch;
-	Texture pipe,invertedPipe,background,bird;
-	OrthographicCamera camera;
+	Texture pipe,invertedPipe,background,birdTexture;
 	
 	int  mode; // 0 running -1 over
 	int x,x1,x2, xBird,yBird ;
@@ -28,11 +28,11 @@ public class myFlappyBird extends ApplicationAdapter {
 	
 	int pipeDistance;
 	int sceneSpeed;
-	int velocity;
 	
 	// libgdx representations of the bird and pipes to detect collisions
-	Circle birdShape;
 	
+	Bird bird;
+
 	Rectangle pipe1Upper,pipe1Lower;
 	Rectangle pipe2Upper,pipe2Lower;
 	Rectangle pipe3Upper,pipe3Lower;
@@ -41,12 +41,10 @@ public class myFlappyBird extends ApplicationAdapter {
 	
 	public myFlappyBird(){
 		
-		velocity = 0;
 		sceneSpeed = 3;
 		pipeDistance = 200;
 		
-		birdShape = new Circle();
-		birdShape.radius = 25;
+		bird = new Bird(25,0,0);
 		
 		pipe1Upper = new Rectangle();
 		pipe1Lower = new Rectangle();
@@ -65,19 +63,15 @@ public class myFlappyBird extends ApplicationAdapter {
 		// create a texture for the pipe image
 		pipe = new Texture("pipe.jpg");
 		invertedPipe = new Texture("invertedPipe.jpg");
-		
-		
-		bird = new Texture("bird.png");
+				
+		birdTexture = new Texture("bird.png");
 		// create a texture for the background image
 		background = new Texture("background.jpg");
 				
 		screenWidth = Gdx.graphics.getWidth();
 		screenHeight = Gdx.graphics.getHeight();
 		
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, screenWidth, screenHeight);
-		
-		batch.setProjectionMatrix(camera.combined);
+
 		min = 200;
 		max = screenHeight - 350;
 		
@@ -96,15 +90,11 @@ public class myFlappyBird extends ApplicationAdapter {
 		pipeHeight1 = min + (int)(Math.random() * ((max - min) + 1));
 		pipeHeight2 = min + (int)(Math.random() * ((max - min) + 1));
 		pipeHeight3 = min + (int)(Math.random() * ((max - min) + 1));
-		
-		
-				
+						
 		xBird = 200;
 		yBird = Gdx.graphics.getHeight()/2;
 		
-		birdShape.x = 200+35;
-		birdShape.y = yBird+35;
-	//	shapeRenderer = new ShapeRenderer();
+		bird.setPosition(xBird + 35, yBird +35);
 	}
 
 	@Override
@@ -112,21 +102,10 @@ public class myFlappyBird extends ApplicationAdapter {
 
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);				
 
-	
 		batch.begin();
-		camera.update();
-		// draw the background
-		batch.draw(background, 0, 0,screenWidth,screenHeight);
-		
-		pipe1Lower.set(x,0,pipeWidth,pipeHeight1);
-		pipe1Upper.set(x,pipeHeight1+ pipeDistance,pipeWidth,screenHeight - pipeHeight1- pipeDistance);
-		
-		pipe2Lower.set(x1,0,pipeWidth,pipeHeight2);
-		pipe2Upper.set(x1,pipeHeight2+ pipeDistance,pipeWidth,screenHeight - pipeHeight2- pipeDistance);
-		
-		pipe3Lower.set(x2,0,pipeWidth,pipeHeight3);
-		pipe3Upper.set(x2,pipeHeight3+ pipeDistance,pipeWidth,screenHeight - pipeHeight3- pipeDistance);
 
+		// draw the background
+		batch.draw(background, 0, 0,screenWidth,screenHeight);		
 		
 		// draw the pipes
 		batch.draw(pipe, x, 0,pipeWidth,pipeHeight1);
@@ -139,61 +118,69 @@ public class myFlappyBird extends ApplicationAdapter {
 		batch.draw(invertedPipe, x2,pipeHeight3+ pipeDistance ,pipeWidth,screenHeight - pipeHeight3- pipeDistance);
 		
 		// draw the bird
-		batch.draw(bird, xBird, yBird,70,70);
+		batch.draw(birdTexture, xBird, yBird,70,70);
 		
 		batch.end();
 	
+		// if the game is over then make the bird fall
 		if(mode < 0){
 			if(yBird > 0)
 				yBird = yBird  - 10;
+				bird.setHeight(yBird);
+				
 			return;
 			
 		}
-		// check if the bird collides with any of the pipes
-		if(Intersector.overlaps(birdShape, pipe1Upper) || Intersector.overlaps(birdShape, pipe1Lower)
-				|| Intersector.overlaps(birdShape, pipe2Upper) || Intersector.overlaps(birdShape, pipe2Lower)
-				|| Intersector.overlaps(birdShape, pipe3Upper) || Intersector.overlaps(birdShape, pipe3Lower)
-				){
-			
-			mode = -1;
+
+		// set the dimensions of the pipes to detect collision
+		pipe1Lower.set(x,0,pipeWidth,pipeHeight1);
+		pipe1Upper.set(x,pipeHeight1+ pipeDistance,pipeWidth,screenHeight - pipeHeight1- pipeDistance);
 		
+		pipe2Lower.set(x1,0,pipeWidth,pipeHeight2);
+		pipe2Upper.set(x1,pipeHeight2+ pipeDistance,pipeWidth,screenHeight - pipeHeight2- pipeDistance);
+		
+		pipe3Lower.set(x2,0,pipeWidth,pipeHeight3);
+		pipe3Upper.set(x2,pipeHeight3+ pipeDistance,pipeWidth,screenHeight - pipeHeight3- pipeDistance);
+
+		// check if the bird collides with any of the pipes
+		if (bird.collides(pipe1Upper) || bird.collides(pipe1Lower) ||
+				bird.collides(pipe2Upper) || bird.collides(pipe2Lower) ||
+				bird.collides(pipe3Upper) || bird.collides(pipe3Lower)){
+		
+			mode = -1;
+			return;
 		}
 		
-		
+		// if the bird did not collide update the screen 
 		x = x - sceneSpeed;
 		if(x < -screenWidth/2){
-		
-//		if(x < (-1 * pipeWidth)){
-				
-			x = Gdx.graphics.getWidth();
-			pipeHeight1 = min + (int)(Math.random() * ((max - min) + 1));
-		
-			
+			x = screenWidth;
+			pipeHeight1 = min + (int)(Math.random() * ((max - min) + 1));		
 		}
 
 		x1 = x1 - sceneSpeed;
-//		if(x1 < (-1 * pipeWidth)){
 		if(x1 < -screenWidth/2){
 			
 			pipeHeight2 = min + (int)(Math.random() * ((max - min) + 1));
-			x1 = Gdx.graphics.getWidth();
+			x1 = screenWidth;
 		}
+
 		x2 = x2 - sceneSpeed;
-//		if(x2 < (-1 * pipeWidth)){
 		if(x2 < -screenWidth/2){
 			pipeHeight3 = min + (int)(Math.random() * ((max - min) + 1));
-			x2 = Gdx.graphics.getWidth();
+			x2 = screenWidth;
 		}	
 		
+		// process the touch input 
+		// whenever the screen is touch move the bird up
 		 if(Gdx.input.isTouched()) {
 			 yBird = yBird  + 10;
-			 birdShape.y = yBird+35;
+			 bird.setHeight(yBird);
 		 }
 		 else{
 			 yBird = yBird  - 4;
-			 birdShape.y = yBird+35;
+			 bird.setHeight(yBird);
 		 }
-		
 		
 	}
 	
