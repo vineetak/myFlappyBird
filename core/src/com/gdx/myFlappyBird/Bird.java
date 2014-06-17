13bc262cf8874a745002832bd0ad7cfc7a748cf6
Vineeta Khatuja <vineetak@Vineetas-MacBook-Pro.local>
2014-06-17 15:31:54 +0530

    Modularized the code, encapsulated bird into a class TODO: add Pipe class

---
+++ core/src/com/gdx/myFlappyBird/Bird.java
@@ -0,0 +1,34 @@
+package com.gdx.myFlappyBird;
+
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Bird {
+
+	Circle shape;
+
+	Bird(int radius,int x , int y){
+		
+		shape = new Circle();
+		shape.radius = radius;
+		shape.x = x;
+		shape.y = y;
+	}
+	
+	boolean collides(Rectangle r){
+		
+		return Intersector.overlaps(shape, r); 
+	}
+	
+	void setHeight(int height){
+		
+		shape.y = height;
+	}
+	
+	void setPosition(int x,int y){
+		
+		shape.x = x;
+		shape.y = y;
+	}
+}
