beda0ec580851b79e9552c927a812f8385552890
Vineeta Khatuja <vineetak@Vineetas-MacBook-Pro.local>
2014-06-17 17:53:46 +0530

    Scene starts moving only after the first touch, bird does not fly with continuous touch input

--- core/src/com/gdx/myFlappyBird/myFlappyBird.java
+++ core/src/com/gdx/myFlappyBird/myFlappyBird.java
@@ -12,11 +12,12 @@ import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 
 public class myFlappyBird extends ApplicationAdapter {
-	
+
 	SpriteBatch batch;
 	Texture pipeTexture,invertedPipe,background,birdTexture;
-	
+
 	int  gameMode; // 0 running -1 gameover
+	int start = -1;
 	
 	int x1,x2,x3, xBird,yBird ;
 	int screenWidth , screenHeight;
@@ -25,64 +26,65 @@ public class myFlappyBird extends ApplicationAdapter {
 	// minimum and maximum pipe height
 	int minPipeHeight ;
 	int maxPipeHeight ;
-	
-	// veritcal distance between upper and lower parts of a pipe
+
+	// vertical distance between upper and lower parts of a pipe
 	int pipeDistance;
-	
+
 	// speed with which the pipes are moving left
 	int sceneSpeed;
-	
+
 	Bird bird;
 	Pipe pipe1,pipe2,pipe3;
-	
+
 	public myFlappyBird(){
-		
+
 		bird = new Bird(25,0,0);
-		
+
 		pipe1 = new Pipe();
 		pipe2 = new Pipe();
 		pipe3 = new Pipe();
 
 		gameMode = 0; // game running
-		
+
 		sceneSpeed = 4;
 		pipeDistance = 200;
 	}
+
 	@Override
 	public void create () {
 		// create a new sprite batch to render the graphics
 		batch = new SpriteBatch();
-		
+
 		// create a texture for the pipes
 		pipeTexture = new Texture("pipe.jpg");
 		invertedPipe = new Texture("invertedPipe.jpg");
-				
+
 		// create the bird texture
 		birdTexture = new Texture("bird.png");
-		
+
 		// create a texture for the background image
 		background = new Texture("background.jpg");
-		
+
 		screenWidth = Gdx.graphics.getWidth();
 		screenHeight = Gdx.graphics.getHeight();
-		
+
 		minPipeHeight = 200;
 		maxPipeHeight = screenHeight - 350;
-			
+
 		pipeWidth = screenWidth / 10;
-		
+
 		x1 = screenWidth;
 		x2 = screenWidth + screenWidth/2 ;
 		x3 = x2  + screenWidth/2;
-		
+
 		// generate the initial pipe heights randomly
 		pipeHeight1 = minPipeHeight + (int)(Math.random() * ((maxPipeHeight - minPipeHeight) + 1));
 		pipeHeight2 = minPipeHeight + (int)(Math.random() * ((maxPipeHeight - minPipeHeight) + 1));
 		pipeHeight3 = minPipeHeight + (int)(Math.random() * ((maxPipeHeight - minPipeHeight) + 1));
-						
+
 		xBird = 200;
 		yBird = screenHeight/2;
-		
+
 		bird.setPosition(xBird+35 , yBird +35);
 	}
 
@@ -91,7 +93,19 @@ public class myFlappyBird extends ApplicationAdapter {
 
 		// draw the Scene
 		drawScene();
-	
+
+		if(Gdx.input.justTouched() ) {
+			start = 1;
+			yBird = yBird  + 75;
+			bird.setHeight(yBird + 35); 
+		}
+		else if(start > 0){
+			yBird = yBird  - 6;
+			bird.setHeight(yBird + 35);
+		}
+		
+		if(start < 0)
+			return;
 		// if the game is already over then make the bird fall and do not update the pipes
 		if(gameMode < 0){
 			if(yBird > 0){
@@ -104,24 +118,17 @@ public class myFlappyBird extends ApplicationAdapter {
 		if(checkForCollision()){
 			return;
 		}
-			
+
 		// update the pipes in the scene
 		updatePipePositions();
-		
+
 		// whenever the screen is touched move the bird upwards
-		 if(Gdx.input.isTouched()) {
-			 yBird = yBird  + 10;
-			 bird.setHeight(yBird + 35); 
-		 }
-		 else{
-			 yBird = yBird  - 4;
-			 bird.setHeight(yBird + 35);
-		 }
 		
+
 	}
-	
+
 	boolean checkForCollision(){
-		
+
 		pipe1.setLowerPipePosition(x1, 0);
 		pipe1.setUpperPipePosition(x1,pipeHeight1+ pipeDistance);
 		pipe1.setLowerHeight(pipeHeight1);
@@ -140,7 +147,7 @@ public class myFlappyBird extends ApplicationAdapter {
 		pipe3.setPipeWidth(pipeWidth);
 		pipe3.setUpperHeight(screenHeight - pipeHeight3 - pipeDistance);
 
-		
+
 		if(bird.collides(pipe1) || bird.collides(pipe2) || bird.collides(pipe3)){
 			gameMode = -1; // game over
 			return true;
@@ -149,7 +156,7 @@ public class myFlappyBird extends ApplicationAdapter {
 		return false;
 	}
 	void updatePipePositions(){
-		 
+
 		x1 = x1 - sceneSpeed;
 		if(x1 < -screenWidth/2){
 			x1 = screenWidth;
@@ -173,27 +180,27 @@ public class myFlappyBird extends ApplicationAdapter {
 	}
 
 	void drawScene(){
-		
+
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 
 		// draw the background
 		batch.draw(background, 0, 0,screenWidth,screenHeight);		
-		
+
 		// draw the pipes
 		batch.draw(pipeTexture, x1, 0,pipeWidth,pipeHeight1);
 		batch.draw(invertedPipe, x1,pipeHeight1+ pipeDistance ,pipeWidth,screenHeight - pipeHeight1- pipeDistance);
-		
+
 		batch.draw(pipeTexture, x2, 0,pipeWidth,pipeHeight2);
 		batch.draw(invertedPipe, x2,pipeHeight2+ pipeDistance ,pipeWidth,screenHeight - pipeHeight2- pipeDistance);
-		
+
 		batch.draw(pipeTexture, x3, 0,pipeWidth,pipeHeight3);
 		batch.draw(invertedPipe, x3,pipeHeight3+ pipeDistance ,pipeWidth,screenHeight - pipeHeight3- pipeDistance);
-		
+
 		// draw the bird
 		batch.draw(birdTexture, xBird, yBird,70,70);
 		batch.end();
-		
+
 		return;
 	}
 }
